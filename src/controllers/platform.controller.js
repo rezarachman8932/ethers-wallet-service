ccc78137d65969098fd1d6e140a7ffb54daa715e
Reza Rachman <rezarachman8932@gmail.com>
2025-11-05 18:04:17 +0700

    WS-05 | Add AuditrailService when calling get platform in the platform controller.

--- src/controllers/platform.controller.js
+++ src/controllers/platform.controller.js
@@ -1,9 +1,19 @@
 const PlatformService = require('../services/platform.service');
+const AuditrailService = require('../services/auditrail.service');
+const AUDIT_ACTION = require('../constants/auditAction.constant');
 const { responseWrapper } = require('../utils/helper');
 const { StatusCodes } = require('http-status-codes');
 
 const getPlatform = async (req, res) => {
     const data = await PlatformService.getAll();
+
+    await AuditrailService.create({
+        action: AUDIT_ACTION.GET_PLATFORM,
+        header: req.headers,
+        body: req.body,
+        ipAddress: req.ip
+    });
+
     return responseWrapper(
         res,
         true,
