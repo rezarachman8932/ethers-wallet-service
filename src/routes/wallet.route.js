82ac1bd48a3fe52b4565c7c065d47847d296cb53
Reza Rachman <reza@knowledgecatalyst.io>
2025-11-18 18:16:34 +0700

    Add new routes.

--- src/routes/wallet.route.js
+++ src/routes/wallet.route.js
@@ -7,5 +7,7 @@ WalletRouter.post("/", authMiddleware, WalletController.createWallet);
 WalletRouter.post('/private', authMiddleware, WalletController.getWalletByPrivateKey);
 WalletRouter.post('/mnemonic', authMiddleware, WalletController.getWalletByMnemonic);
 WalletRouter.post('/balance', authMiddleware, WalletController.getWalletBalance);
+WalletRouter.post('/transaction/history', authMiddleware, WalletController.getTransactionHistory);
+WalletRouter.post('/transaction/detail', authMiddleware, WalletController.getTransactionDetail);
 
 module.exports = WalletRouter;
