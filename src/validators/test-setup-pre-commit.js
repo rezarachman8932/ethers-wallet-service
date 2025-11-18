03d1bc2e85bdaa7dbabf1d3b67874b7425ecb84f
ranggakc <rangga@knowledgecatalyst.io>
2025-11-18 15:30:20 +0700

    chore: testing pre commit hooks

---
+++ src/validators/test-setup-pre-commit.js
@@ -0,0 +1,9 @@
+const testFunctionForPreCommitHooks = () => {
+  // Setup pre-commit hooks
+  console.log('Testing pre-commit hooks...');
+  return;
+};
+
+module.exports = {
+  testFunctionForPreCommitHooks,
+};
